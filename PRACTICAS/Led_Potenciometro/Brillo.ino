d6332b0c9f3486954eb754ac08df0a5ab538d8ab
Leito_uwu <leonardo.vargas.c@ucb.edu.bo>
2024-08-26 18:57:03 -0400

    Actualizacion

--- PRACTICAS/Led_Potenciometro/Brillo.ino
+++ PRACTICAS/Led_Potenciometro/Brillo.ino
@@ -9,6 +9,10 @@ void setup()
 
 void loop()
 {
-  int brillo = analogRead(POT)/4;
+  //int brillo = analogRead(POT)/4;         Puede ser una opcion
+  int A = analogRead(POT);
+  //para la funcion map se definen 5 argumentos el primero que es el valor del pot
+  //los otros dos son el minimo y maximo del valor, despues la conversion a la que se quiere realizar en este caso es de 0-255
+  int brillo = map(A, 0, 2023, 0 , 255);
   analogWrite(LED,brillo);
 }
