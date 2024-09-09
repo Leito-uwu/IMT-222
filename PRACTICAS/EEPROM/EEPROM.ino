e6940e7a64497426d2a4aed8a4855afdfaa099a5
Leito_uwu <leonardo.vargas.c@ucb.edu.bo>
2024-09-09 19:55:14 -0400

    primer_codigo

---
+++ PRACTICAS/EEPROM/EEPROM.ino
@@ -0,0 +1,4 @@
+#include <EEPROM.h>
+
+EEPROM.read(address);            //Varias los datos desde 0-2023 datos
+EEPROM.write(address,value);     //puedes leer un valor predeteminado en una direccion dada
